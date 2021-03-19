b8e31f859a9782ea648d477f415a4b999ab8b5de
lilyasamsonova <lilyasamsonova@gmail.com>
2021-03-19 09:54:05 +0300

    Create filter.js

---
+++ js/filter.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+function activateFilterAnimation() {
+  $("body").toggleClass("animated1");
+}
+
+$(".bt1").on("click", activateFilterAnimation);
+})
+
+// $(document).ready(function(){
+//
+// function activateFilterAnimation() {
+//   $(".second").toggleClass("animated2");
+// }
+//
+// $(".bt2").on("click", activateFilterAnimation);
+// })
