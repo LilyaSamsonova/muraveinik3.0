38b1ecb01491f1182d847777cedcf035dbabab38
lilyasamsonova <lilyasamsonova@gmail.com>
2021-03-19 09:54:08 +0300

    Create colors.js

---
+++ js/colors.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+// $('.bt2').click(
+//       function(){
+//           $('.eye').css('background-color','red');
+//       },
+//       function(){
+//           $('.eye').css('background-color','yellow');
+//       },
+//   );
+$(".bt2").on("click", function() {
+        // $(".eye").css('color') === '#f71e1e' ?
+        // $(".eye").css('color', '#333333') :
+        $(".eye").css('color', '#f71e1e');
+    });
