e0fc9d7dacf811e0bad2b67a430ddc5332870d79
lilyasamsonova <lilyasamsonova@gmail.com>
2021-03-26 12:45:48 +0300

    Create info.js

---
+++ js/info.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//
+// // $('.btinfo').on('click', function() {
+// //     if($('.fourth').is(':visible')) {
+// //         $('.fourth').slideUp(400);
+// //     } else {
+// //         $('.fourth').slideDown(400);
+// //     }
+// // });
+//
+// function down()
+// {
+// var a = document.querySelectorAll('.aaa');
+// for(var i = 0, l = a.length; i < l; i++){
+// if ( a[i].style.display == 'none' )
+// a[i].style.display = 'block'
+// else
+// a[i].style.display = 'none';
+// }
+// }
